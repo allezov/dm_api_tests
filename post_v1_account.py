30cba088db9caa838188835fb241ba57c72b1a04
lezov <al.lezov@yandex.ru>
2023-04-26 17:23:54 +0400

    Переложил в PyCharm и обернул в функции  вызовы из Postman согласно заданию

--- post_v1_account.py
+++ post_v1_account.py
@@ -1,20 +1,33 @@
 import requests
-import json
 
-url = "http://localhost:5051/v1/account"
 
-payload = json.dumps({
-  "login": "<test3>",
-  "email": "<test3@test.ru>",
-  "password": "<testtest3>"
-})
-headers = {
-  'X-Dm-Auth-Token': '<string>',
-  'X-Dm-Bb-Render-Mode': '<string>',
-  'Content-Type': 'application/json',
-  'Accept': 'text/plain'
-}
+def post_v1_account():
+    """
+    Register new user
+    :return:
+    """
+    url = "http://localhost:5051/v1/account"
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    payload = {
+        "login": "<test4>",
+        "email": "<test3@test.ru>",
+        "password": "<testtest3>"
+    }
+    headers = {
+        'X-Dm-Auth-Token': '<string>',
+        'X-Dm-Bb-Render-Mode': '<string>',
+        'Content-Type': 'application/json',
+        'Accept': 'text/plain'
+    }
 
-print(response.text)
+    response = requests.request(
+        method="POST",
+        url=url,
+        headers=headers,
+        json=payload
+    )
+    return response
+
+
+
+

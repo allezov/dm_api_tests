df2e18f20964914b1386da2313ee66337d6ab4ce
Aleksandr Lezov <al.lezov@yandex.ru>
2023-06-13 17:38:47 +0600

    Собрал зависимости для ветки clients

--- tests/tests_account/test_post_v1_account_password.py
+++ tests/tests_account/test_post_v1_account_password.py
@@ -12,6 +12,5 @@ def test_post_v1_account_password(dm_api_facade, prepare_user,assertion):
     assertion.check_user_was_created_for_prepare(login=login)
     dm_api_facade.account.activate_registered_user(login=login)
     token = dm_api_facade.login.get_auth_token(login=login, password=password)
-
     dm_api_facade.account.set_headers(headers=token)
     dm_api_facade.account.reset_user_password(login=login, email=email)
--- tests/tests_account/test_put_v1_account_password.py
+++ tests/tests_account/test_put_v1_account_password.py
@@ -13,19 +13,12 @@ def test_put_v1_account_password(dm_api_facade, prepare_user, mailhog):
         password=password,
         status_code=201
     )
-
     dm_api_facade.account.activate_registered_user(login=login)
-
     token = dm_api_facade.login.get_auth_token(login=login, password=password)
     dm_api_facade.account.set_headers(headers=token)
-
     dm_api_facade.account.reset_user_password(login=login, email=email)
     time.sleep(2)
     token = mailhog.get_token_from_last_email()
-
-    print(token)
-    print(type(token))
-
     dm_api_facade.account.change_user_password(
         login=login,
         token=token,
@@ -33,6 +26,3 @@ def test_put_v1_account_password(dm_api_facade, prepare_user, mailhog):
         new_password=new_password
     )
 
-    # Немного схитрил и поменял в моделе change_password тип на StrictStr(было UUID)
-    # token: Optional[StrictStr] = Field(None, description='Password reset token')
-    # TypeError: Object of type UUID is not JSON serializable
